c776e11dd9976995e9edf288d82a6ee38c03394e
unknown <christianlugod05@gmail.com>
2019-12-14 00:39:47 -0800

    [Front End] Wishlist page, Quick Category, Cards Pagination Added

--- resources/js/image-viewer.js
+++ resources/js/image-viewer.js
@@ -1,21 +1,24 @@
-$(document).ready(function () {
-
-    $('#show-list').not('.slick-initialized').slick({
-        slidesToShow: 5,
-        vertical: true,
-        arrows: true,
-        dot: false,
-        swipe: true,
-        asNavFor: '#show-main',
-        verticalSwiping: true,
-        focusOnSelect: true,
-    });
-    $('#show-main').not('.slick-initialized').slick({
-        slidesToShow: 1,
-        arrows: true,
-        dot: true,
-        fade: true,
-        asNavFor: '#show-list',
-    });
-
+$(document).ready(function() {
+	$('#show-list')
+		.not('.slick-initialized')
+		.slick({
+			// slidesToShow: 5,
+			vertical: true,
+			arrows: true,
+			dot: false,
+			swipe: true,
+			asNavFor: '#show-main',
+			verticalSwiping: true,
+			focusOnSelect: true,
+			centerMode: true
+		});
+	$('#show-main')
+		.not('.slick-initialized')
+		.slick({
+			// slidesToShow: 1,
+			arrows: true,
+			dot: true,
+			fade: true,
+			asNavFor: '#show-list'
+		});
 });
