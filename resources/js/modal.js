c265b3d1cfdb2aebcfe8004d5545c54111bdb31e
unknown <christianlugod05@gmail.com>
2019-12-14 22:12:45 -0800

    [Front End] Account and Collections Page Added

--- resources/js/modal.js
+++ resources/js/modal.js
@@ -1,26 +1,40 @@
-let modal = document.getElementById("over-modal");
-let modal_exit = document.getElementById("modal-exit");
+let modal = document.getElementById('over-modal');
+let modal_exit = document.getElementById('modal-exit');
 
-modal_exit.addEventListener("click", () => purchase_close());
+modal_exit.addEventListener('click', () => purchase_close());
 
-modal.addEventListener("keypress", e => {
-  console.log("press");
-  if (e.keyCode == 27) {
-    purchase_close();
-  }
+modal.addEventListener('keypress', e => {
+	console.log('press');
+	if (e.keyCode == 27) {
+		purchase_close();
+	}
 });
 
 function purchase_modal() {
-  modal.classList.add("modal-show");
-  modal.focus();
-  scroll_lock("hidden");
+	modal.classList.add('modal-show');
+	modal.focus();
+	scroll_lock('hidden');
 }
 
 function purchase_close() {
-  modal.classList.remove("modal-show");
-  scroll_lock("auto");
+	modal.classList.remove('modal-show');
+	scroll_lock('auto');
 }
 
 function scroll_lock(mode) {
-  document.body.style.overflow = mode;
+	document.body.style.overflow = mode;
 }
+
+let search = document.getElementById('search-modal');
+let search_exit = document.getElementById('search-exit');
+let search_open = document.getElementById('btn-search');
+
+search_exit.addEventListener('click', () => (search.style.display = 'none'));
+search_open.addEventListener('click', () => (search.style.display = 'flex'));
+
+let cart = document.getElementById('cart-modal');
+// let card_exit = document.getElementById('cart-exit');
+// let card_open = document.getElementById('btn-cart');
+
+// card_exit.addEventListener('click', () => (cart.style.display = 'none'));
+// card_open.addEventListener('click', () => (cart.style.display = 'block'));
